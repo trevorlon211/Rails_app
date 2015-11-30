39ae53c613cdc259092f064d2725f8d0d6c15e43
trevorlon211 <trevorlon.211@gmail.com>
2015-11-29 23:23:30 -0500

    grid to products page

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151129171356) do
+ActiveRecord::Schema.define(version: 20151129174559) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20151129171356) do
     t.string   "image_url"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
-    t.string   "color"
+    t.string   "colour"
   end
 
 end
