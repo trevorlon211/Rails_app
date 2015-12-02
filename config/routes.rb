1995e102eda9e3684fd40bc93b1a1356986fdad9
trevorlon211 <trevorlon.211@gmail.com>
2015-12-01 23:32:54 -0500

    controllers

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#landing_page'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
