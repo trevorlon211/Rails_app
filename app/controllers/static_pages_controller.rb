e6434ed1d2ff23f63d473e43c20401f741bb1928
trevorlon211 <trevorlon.211@gmail.com>
2015-12-19 11:28:30 -0500

    mails

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,3 +5,10 @@ class StaticPagesController < ApplicationController
   end
 
 end
+
+def thank_you
+  @name = params[:name]
+  @email = params[:email]
+  @message = params[:message]
+  UserMailer.contact_form(@email, @name, @message).deliver_now
+end
