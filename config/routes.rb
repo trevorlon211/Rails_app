a428706d5c7c832badb7b179cca9f49a93eb56e8
trevorlon211 <trevorlon.211@gmail.com>
2016-01-09 00:33:41 -0500

    rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  
+  resources :products do
+    resources :comments
+  end
   resources :users
-  resources :products
+  
   get 'static_pages/about'
 
   get 'static_pages/contact'
