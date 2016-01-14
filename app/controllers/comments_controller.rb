a49ea76a28ff8f30819517b6f4ce9b90592a29ff
trevorlon211 <trevorlon.211@gmail.com>
2016-01-13 23:30:43 -0500

    rate

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
-
-  def create
+	def create
 		@product = Product.find(params[:product_id])
 		@comment = @product.comments.new(comment_params)
 		@comment.user = current_user
@@ -8,6 +7,7 @@ class CommentsController < ApplicationController
 			if @comment.save
 				format.html { redirect_to @product, notice: 'Review was created successfully.' }
 				format.json { render :show, status: :created, location: @product }
+				format.js
 			else
 				format.html { redirect_to @product, alert: 'Review was not saved successfully' }
 				format.json { render json: @comment.errors, status: :unprocessable_entity }
@@ -15,10 +15,16 @@ class CommentsController < ApplicationController
 		end
 	end
 
-  def destroy
-  end
 
-  private
+
+	def destroy
+		@comment = Comment.find(params[:id])
+		product = @comment.product
+		@comment.destroy
+		redirect_to product
+	end
+
+	private
 
 		def comment_params
 			params.require(:comment).permit(:user_id, :body, :rating)
