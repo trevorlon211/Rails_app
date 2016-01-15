4ce40718a63d9a6f87e18c06ecda09dbe93eae8d
trevorlon211 <trevorlon.211@gmail.com>
2016-01-14 23:00:00 -0500

    comments

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,9 +1,9 @@
 $(document).on('ready page:load', function(){
-    $('.rating').raty( { path: '/assets', scoreName: 'comment[rating]' });
-    $('.rated').raty({ path: '/assets',
-      readOnly: true,
-      score: function() {
-        return $(this).attr('data-score');
-      }
-    });
+	$('.rating').raty({path: '/assets/raty', scoreName: 'comment[rating]'});
+	$('.rated').raty({path: '/assets/raty',
+		readOnly: true,
+		score: function() {
+			return $(this).attr('data-score');
+		}
+	});
 });
