2b1a8c0f4512725212d1e46e1ce5f37d4b6eaf03
trevorlon211 <trevorlon.211@gmail.com>
2016-01-20 14:40:53 -0500

    edits

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,5 +7,11 @@ class UserMailer < ApplicationMailer
         :to => 'trevorlon.211@gmail.com',
         :subject => "A new contact form message from #{name}")
   end
+
+  def welcome(user)
+  	@appname = "Imaginatepics"
+  	mail( :to => user.email,
+  		  :subject => "Welcome to #{@appname}!")
+  end
 end
 
