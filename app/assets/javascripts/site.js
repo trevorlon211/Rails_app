663bc8b2f94b19a3f8186fa159cda1d6ed243819
trevorlon211 <trevorlon.211@gmail.com>
2016-01-21 14:16:19 -0500

    rating

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,7 +1,6 @@
 $(document).on('ready page:load', function(){
-	/*! raty */
-	$('.rating').raty({path: '/assets/images/raty', scoreName: 'comment[rating]'});
-	$('.rated').raty({path: '/assets/images/raty',
+	$('.rating').raty({path: '/assets/raty', scoreName: 'comment[rating]'});
+	$('.rated').raty({path: '/assets/raty',
 		readOnly: true,
 		score: function() {
 			return $(this).attr('data-score');
