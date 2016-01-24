b3c050c4c9a268f0e4782fff14e73b82fad94265
trevorlon211 <trevorlon.211@gmail.com>
2016-01-23 23:45:49 -0500

    nav

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -6,11 +6,6 @@ $(document).on('ready page:load', function(){
 			return $(this).attr('data-score');
 		}
 	});
+
 });
 
-var map;
-   function initMap() {
-   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 40.7383593, lng: -73.9863218},
-  });
- }
