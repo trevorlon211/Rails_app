340903ebc032b4eb2dc3876fd2abdba661cf5f46
trevorlon211 <trevorlon.211@gmail.com>
2016-01-23 22:23:00 -0500

    contact

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -7,3 +7,10 @@ $(document).on('ready page:load', function(){
 		}
 	});
 });
+
+var map;
+   function initMap() {
+   map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.7383593, lng: -73.9863218},
+  });
+ }
