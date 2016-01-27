897008d8fde53a870d297a2cd3720be69d769ba1
trevorlon211 <trevorlon.211@gmail.com>
2016-01-26 23:14:32 -0500

    unit tests

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,10 +6,18 @@ describe Product do
 			@product = Product.create!(name: "race bike")
 			@user = User.create(email: "test@email.com", password: "12345678")
 			@product.comments.create!(rating: 1, user: @user, body: "Awful bike!")
+			@product.comments.create!(rating: 3, user: @user, body: "OK Bike")
+            @product.comments.create!(rating: 5, user: @user, body: "Nice Bike")
 	     end
-	 end
+
 	it "returns the average rating of all comments" do
 		expect(@product.average_rating).to eq 3.0
 	end
+
+	it "validates the products" do
+		expect(Product.new(description: "Nice bike")).not_to be_valid
+	end
 	
+  end
 end
+	
