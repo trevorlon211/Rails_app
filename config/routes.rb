cfde780f4170e8c8969918dae68a840256dce84a
trevorlon211 <trevorlon.211@gmail.com>
2016-02-23 15:56:10 -0500

    edits

--- config/routes.rb
+++ config/routes.rb
@@ -26,7 +26,7 @@ Rails.application.routes.draw do
 
   post 'payments/create'
 
-  resources :orders, only: [:index, :show, :create, :delete]
+  resources :orders, only: [:index, :show, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
