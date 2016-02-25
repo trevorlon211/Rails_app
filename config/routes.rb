8a851ed176508331962878733828cbef28e7773b
trevorlon211 <trevorlon.211@gmail.com>
2016-02-24 23:14:09 -0500

    edits

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users, controllers: {
-    registrations: "users/registrations"
-  }
+  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}, 
+             :controllers => { :registrations => "user_registrations" }
+             
   
   resources :users
   resources :products do
